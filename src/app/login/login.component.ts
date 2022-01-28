b01d7e968e3114495ef8af5e3dbbf698d4e9b441
Léo Rolland <leo.rolland@spikeelabs.fr>
2022-01-28 17:46:55 +0100

    feat(login): login, token & localstorage, interceptor, redirections

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiHelperService } from '../services/api-helper.service';
+import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,9 @@ import { ApiHelperService } from '../services/api-helper.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private api: ApiHelperService) { }
+  waitingForResponse = false
+
+  constructor(private api: ApiHelperService, private tokenStorageService: TokenStorageService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -16,7 +20,18 @@ export class LoginComponent implements OnInit {
   login(): void {
     const username: string = (document.getElementById('username') as HTMLInputElement).value;
     const password: string = (document.getElementById('password') as HTMLInputElement).value;
-    this.api.post({endpoint: '/auth/login', data: { username, password },}).then(response => console.log(response));
+    this.waitingForResponse = true;
+    this.api.post({endpoint: '/auth/login', data: { username, password },})
+      .catch(e => alert(e.message))
+      .then(response => this.tokenStorageService.save(response.access_token))
+      .then(() => {
+        if (this.tokenStorageService.isLogged()) {
+          this.router.navigateByUrl('/users')
+        } else {
+
+        }
+      })
+      .finally(() => this.waitingForResponse = false)
   }
 
 }
