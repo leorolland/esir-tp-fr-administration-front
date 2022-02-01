594b728a5cce7614b5d46917e94efa6dd9769862
Léo Rolland <leo.rolland@spikeelabs.fr>
2022-02-01 08:29:14 +0100

    feat(guard): created login guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { UsersListComponent } from './users-list/users-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'users', component: UsersListComponent },
+  { path: 'users', component: UsersListComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'login' },
 ];
 
